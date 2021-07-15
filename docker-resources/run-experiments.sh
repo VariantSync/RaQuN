f3aac58aef1cd9a7ce4449c8b45d9d4f014fef47
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-07-15 09:25:32 +0200

    Updated run script

--- docker-resources/run-experiments.sh
+++ docker-resources/run-experiments.sh
@@ -29,21 +29,28 @@ else
   echo "Files in WORKDIR"
   ls -l
   echo ""
+  
+  valid_argument=false
 
-  if [ "$1" == 'run RQ1' ]
+  if [ "$1" == 'RQ1' ] || [ "$1" == 'run' ]
   then
       echo "Running experiment for RQ1"
       java -jar RQ1Runner-jar-with-dependencies.jar experiment.properties
-  elif [ "$1" == 'run RQ2' ]
+      valid_argument=true
+  fi
+  if [ "$1" == 'RQ2' ] || [ "$1" == 'run' ]
   then
       echo "Running experiment for RQ2"
-
       java -jar RQ2Runner-jar-with-dependencies.jar experiment.properties
-  elif [ "$1" == 'run RQ3' ]
+      valid_argument=true
+  fi
+  if [ "$1" == 'RQ3' ] || [ "$1" == 'run' ]
   then
       echo "Running experiment for RQ3"
       java -jar RQ3Runner-jar-with-dependencies.jar experiment.properties
-  elif [ "$1" == 'validate' ]
+      valid_argument=true
+  fi
+  if [ "$1" == 'validate' ]
   then
       echo "Running a 10 minute validation."
       java -jar RQ1Runner-jar-with-dependencies.jar quick-validation.properties
@@ -52,13 +59,15 @@ else
       echo "Running result evaluation"
       cd result_analysis_python || exit
       python3.8 evaluation.py
-  else
+      valid_argument=true
+  fi
+  if [ $valid_argument == true ]
+  then
       echo "Either fully run a specific experiment one time (run [RQ1|RQ2|RQ3]) or evaluate the results (evaluate) or a do quick setup validation (validate)."
       echo "-- Bash Examples --"
       echo "Run RQ1: './experiments.sh run RQ1'"
       echo "Run RQ2: './experiments.sh run RQ2'"
       echo "Evaluate all gathered results: './experiments.sh evaluate'"
       echo "Validate the setup: './experiments.sh validate'"
-  exit
   fi
 fi
