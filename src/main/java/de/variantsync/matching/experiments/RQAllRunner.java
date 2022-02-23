e067e458a320e6dd35a1a314177275310f51e348
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2022-02-23 19:11:32 +0100

    Added final results

--- src/main/java/de/variantsync/matching/experiments/RQAllRunner.java
+++ src/main/java/de/variantsync/matching/experiments/RQAllRunner.java
@@ -2,11 +2,12 @@ package de.variantsync.matching.experiments;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class RQAllRunner extends AbstractRQRunner{
+public class RQAllRunner extends AbstractRQRunner {
     private static final int NUM_THREADS = 5;
 
     public RQAllRunner() {
@@ -20,8 +21,8 @@ public class RQAllRunner extends AbstractRQRunner{
     private static void waitAndClear(final List<Future<?>> futures) {
         futures.forEach(f -> {
             try {
-                f.wait();
-            } catch (final InterruptedException e) {
+                f.get();
+            } catch (final InterruptedException | ExecutionException e) {
                 throw new RuntimeException(e);
             }
         });
@@ -35,41 +36,44 @@ public class RQAllRunner extends AbstractRQRunner{
         final ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
 
         // Run RQ1
-        for (int i = 0; i < configuration.repetitionsRQ1(); i++) {
+        for (int i = 0; i < 30; i++) {
             futures.add(executorService.submit(() -> new RQ1Runner(configPath).run()));
         }
         waitAndClear(futures);
+        System.out.println("Done with RQ1");
 
         // Run RQ2
-        for (int i = 0; i < configuration.repetitionsRQ2(); i++) {
+        for (int i = 0; i < 30; i++) {
             futures.add(executorService.submit(() -> new RQ2Runner(configPath).run()));
         }
         waitAndClear(futures);
+        System.out.println("Done with RQ2");
 
         // Run RQ3
         // For each ArgoUML subset slice
         for (int i = 1; i <= 30; i++) {
-            for (int j = 0; j < configuration.repetitionsRQ3(); j++) {
-                final String subsetId = String.valueOf(i);
-                futures.add(executorService.submit(() -> new RQ3Runner(configPath, subsetId).run()));
-            }
+            final String subsetId = String.valueOf(i);
+            futures.add(executorService.submit(() -> new RQ3Runner(configPath, subsetId).run()));
         }
         waitAndClear(futures);
+        System.out.println("Done with RQ3");
 
         // Run RQ4
-        for (int i = 0; i < configuration.repetitionsRQ3(); i++) {
+        for (int i = 0; i < 30; i++) {
             futures.add(executorService.submit(() -> new RQ4Runner(configPath).run()));
         }
         waitAndClear(futures);
+        System.out.println("Done with RQ4");
 
         // Run RQ5
         // For each ArgoUML subset slice
         for (int i = 1; i <= 30; i++) {
-            for (int j = 0; j < configuration.repetitionsRQ5(); j++) {
-                final String subsetId = String.valueOf(i);
-                futures.add(executorService.submit(() -> new RQ5Runner(configPath, subsetId).run()));
-            }
+            final String subsetId = String.valueOf(i);
+            futures.add(executorService.submit(() -> new RQ5Runner(configPath, subsetId).run()));
         }
         waitAndClear(futures);
+        System.out.println("Done with RQ5");
+
+        executorService.shutdown();
     }
 }
